cacef98305b1bdcca6e9f9fa053e116ca6fdcb5b
Amir12323e <eyadm7191@gmail.com>
2026-04-16 05:01:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,25 @@ st.title("🧠 Einstein AI Chatbot")
 generator = pipeline("text-generation", model="gpt2")
 
 def chat(question):
-    prompt = f"You are Albert Einstein. Answer wisely: {question}"
-    
+    if question.strip() == "":
+        return "من فضلك اكتب سؤال"
+
+    prompt = f"""
+    أنت العالم ألبرت أينشتاين.
+    أجب على السؤال التالي باللغة العربية بأسلوب بسيط وذكي:
+
+    السؤال: {question}
+    """
+
     result = generator(
         prompt,
-        max_length=100,
+        max_length=120,
         do_sample=True,
         temperature=0.7,
         top_k=50,
         top_p=0.9
     )
-    
+
     return result[0]['generated_text']
 
 question = st.text_input("Ask Einstein:")
